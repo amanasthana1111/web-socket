84ea639bd2d143dc37ebbac8b294293326eca68e
amanasthana1111 <as.asthana.1111@gmail.com>
2026-01-19 14:14:38 +0530

    web socket prep

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("e")
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//event handler
+wss.on("connection", (socket) => {
+//   setInterval(() => {
+//     socket.send("hello");
+//   }, 500);/
+socket.on("message",(e)=>{
+    // console.log(e.toString())
+    // socket.send("hello")
+})
+});
