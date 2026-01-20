8fe1a3122fa16da86a9d39d9014998ed46ad52f0
amanasthana1111 <as.asthana.1111@gmail.com>
2026-01-20 11:08:42 +0530

    maked ping pong server

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-//event handler
+
 wss.on("connection", (socket) => {
 //   setInterval(() => {
 //     socket.send("hello");
 //   }, 500);/
 socket.on("message",(e)=>{
-    // console.log(e.toString())
-    // socket.send("hello")
+    
+if(e.toString() === "ping"){
+    socket.send("pong")
+}
 })
 });
 
